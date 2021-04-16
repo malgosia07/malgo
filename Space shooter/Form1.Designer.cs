0b2dcf1f5bfd6fe1ece457f1cea6e0bf6af1293d
malgosia07 <73280105+malgosia07@users.noreply.github.com>
2021-04-16 18:24:04 +0200

    Add files via upload

---
+++ Space shooter/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace Space_shooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Move_timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxhero_player = new System.Windows.Forms.PictureBox();
+            this.timer_prawo = new System.Windows.Forms.Timer(this.components);
+            this.timer_lewo = new System.Windows.Forms.Timer(this.components);
+            this.timer_wGore = new System.Windows.Forms.Timer(this.components);
+            this.timer_wDol = new System.Windows.Forms.Timer(this.components);
+            this.timerPocisk = new System.Windows.Forms.Timer(this.components);
+            this.timerPrzeciwnik = new System.Windows.Forms.Timer(this.components);
+            this.timerPrzeciwnik_Pocisk = new System.Windows.Forms.Timer(this.components);
+            this.label = new System.Windows.Forms.Label();
+            this.btn_powtorz = new System.Windows.Forms.Button();
+            this.btn_zakoncz = new System.Windows.Forms.Button();
+            this.label_poziom = new System.Windows.Forms.Label();
+            this.label_punkty = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxhero_player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Move_timer
+            // 
+            this.Move_timer.Enabled = true;
+            this.Move_timer.Interval = 10;
+            this.Move_timer.Tick += new System.EventHandler(this.Move_timer_Tick);
+            // 
+            // pictureBoxhero_player
+            // 
+            this.pictureBoxhero_player.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxhero_player.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxhero_player.Image")));
+            this.pictureBoxhero_player.Location = new System.Drawing.Point(255, 490);
+            this.pictureBoxhero_player.Name = "pictureBoxhero_player";
+            this.pictureBoxhero_player.Size = new System.Drawing.Size(50, 50);
+            this.pictureBoxhero_player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxhero_player.TabIndex = 0;
+            this.pictureBoxhero_player.TabStop = false;
+            this.pictureBoxhero_player.Click += new System.EventHandler(this.pictureBoxhero_player_Click);
+            // 
+            // timer_prawo
+            // 
+            this.timer_prawo.Interval = 5;
+            this.timer_prawo.Tick += new System.EventHandler(this.timer_prawo_Tick);
+            // 
+            // timer_lewo
+            // 
+            this.timer_lewo.Interval = 5;
+            this.timer_lewo.Tick += new System.EventHandler(this.timer_lewo_Tick);
+            // 
+            // timer_wGore
+            // 
+            this.timer_wGore.Interval = 5;
+            this.timer_wGore.Tick += new System.EventHandler(this.timer_wGore_Tick);
+            // 
+            // timer_wDol
+            // 
+            this.timer_wDol.Interval = 5;
+            this.timer_wDol.Tick += new System.EventHandler(this.timer_wDol_Tick);
+            // 
+            // timerPocisk
+            // 
+            this.timerPocisk.Enabled = true;
+            this.timerPocisk.Interval = 25;
+            this.timerPocisk.Tick += new System.EventHandler(this.timerPocisk_Tick);
+            // 
+            // timerPrzeciwnik
+            // 
+            this.timerPrzeciwnik.Enabled = true;
+            this.timerPrzeciwnik.Tick += new System.EventHandler(this.timerPrzeciwnik_Tick);
+            // 
+            // timerPrzeciwnik_Pocisk
+            // 
+            this.timerPrzeciwnik_Pocisk.Enabled = true;
+            this.timerPrzeciwnik_Pocisk.Interval = 20;
+            this.timerPrzeciwnik_Pocisk.Tick += new System.EventHandler(this.timerPrzeciwnik_Pocisk_Tick);
+            // 
+            // label
+            // 
+            this.label.BackColor = System.Drawing.Color.Transparent;
+            this.label.Font = new System.Drawing.Font("Agency FB", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label.Location = new System.Drawing.Point(168, 212);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(264, 84);
+            this.label.TabIndex = 1;
+            this.label.Text = "Koniec gry";
+            this.label.Visible = false;
+            this.label.Click += new System.EventHandler(this.label_Click);
+            // 
+            // btn_powtorz
+            // 
+            this.btn_powtorz.BackColor = System.Drawing.Color.AliceBlue;
+            this.btn_powtorz.Font = new System.Drawing.Font("Arial Black", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_powtorz.Location = new System.Drawing.Point(59, 299);
+            this.btn_powtorz.Name = "btn_powtorz";
+            this.btn_powtorz.Size = new System.Drawing.Size(221, 42);
+            this.btn_powtorz.TabIndex = 2;
+            this.btn_powtorz.Text = "Powtórz";
+            this.btn_powtorz.UseVisualStyleBackColor = false;
+            this.btn_powtorz.Visible = false;
+            this.btn_powtorz.Click += new System.EventHandler(this.btn_powtorz_Click);
+            // 
+            // btn_zakoncz
+            // 
+            this.btn_zakoncz.BackColor = System.Drawing.Color.AliceBlue;
+            this.btn_zakoncz.Font = new System.Drawing.Font("Arial Black", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_zakoncz.Location = new System.Drawing.Point(286, 300);
+            this.btn_zakoncz.Name = "btn_zakoncz";
+            this.btn_zakoncz.Size = new System.Drawing.Size(221, 41);
+            this.btn_zakoncz.TabIndex = 3;
+            this.btn_zakoncz.Text = "Zakończ";
+            this.btn_zakoncz.UseVisualStyleBackColor = false;
+            this.btn_zakoncz.Visible = false;
+            this.btn_zakoncz.Click += new System.EventHandler(this.btn_zakoncz_Click);
+            // 
+            // label_poziom
+            // 
+            this.label_poziom.AutoSize = true;
+            this.label_poziom.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_poziom.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label_poziom.Location = new System.Drawing.Point(540, 9);
+            this.label_poziom.Name = "label_poziom";
+            this.label_poziom.Size = new System.Drawing.Size(30, 24);
+            this.label_poziom.TabIndex = 4;
+            this.label_poziom.Text = "01";
+            // 
+            // label_punkty
+            // 
+            this.label_punkty.AutoSize = true;
+            this.label_punkty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_punkty.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label_punkty.Location = new System.Drawing.Point(92, 9);
+            this.label_punkty.Name = "label_punkty";
+            this.label_punkty.Size = new System.Drawing.Size(30, 24);
+            this.label_punkty.TabIndex = 5;
+            this.label_punkty.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Punkty:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(461, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Pozim:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(582, 553);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_punkty);
+            this.Controls.Add(this.label_poziom);
+            this.Controls.Add(this.btn_zakoncz);
+            this.Controls.Add(this.btn_powtorz);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.pictureBoxhero_player);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(600, 600);
+            this.MinimumSize = new System.Drawing.Size(600, 600);
+            this.Name = "Form1";
+            this.Text = "Space shooter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxhero_player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Move_timer;
+        private System.Windows.Forms.PictureBox pictureBoxhero_player;
+        private System.Windows.Forms.Timer timer_prawo;
+        private System.Windows.Forms.Timer timer_lewo;
+        private System.Windows.Forms.Timer timer_wGore;
+        private System.Windows.Forms.Timer timer_wDol;
+        private System.Windows.Forms.Timer timerPocisk;
+        private System.Windows.Forms.Timer timerPrzeciwnik;
+        private System.Windows.Forms.Timer timerPrzeciwnik_Pocisk;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button btn_powtorz;
+        private System.Windows.Forms.Button btn_zakoncz;
+        private System.Windows.Forms.Label label_poziom;
+        private System.Windows.Forms.Label label_punkty;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Space shooter/Form1.cs
@@ -0,0 +1,461 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Space_shooter
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer muzykaWtle;
+        WindowsMediaPlayer dzwiek_pocisku;
+        WindowsMediaPlayer eksplozja;
+
+        PictureBox[] Gwiazdy;
+        PictureBox[] Pocisk;
+        PictureBox[] Przeciwnicy;
+        PictureBox[] Pocisk_Przeciwnika;
+
+        int szybkosc_pocisk_przeciwnika;
+        int szybkosc_pocisku;
+        int szybkosc_tla;
+        int szybkosc_gracza;
+        int szybkosc_przeciwnika;
+
+        int punkty;
+        int poziom;
+        int poziom_trudnisci;
+        bool pausa;
+        bool koniec;
+
+        Random rand;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pausa = false;
+            koniec = false;
+            punkty = 0;
+            poziom = 1;
+            poziom_trudnisci = 10;
+
+            szybkosc_tla = 4;
+            szybkosc_gracza = 5;
+            szybkosc_pocisku = 10;
+            szybkosc_przeciwnika = 8;
+            szybkosc_pocisk_przeciwnika = 10;
+            
+          
+            muzykaWtle = new WindowsMediaPlayer();
+            dzwiek_pocisku = new WindowsMediaPlayer();
+            eksplozja = new WindowsMediaPlayer();
+
+            muzykaWtle.URL = "amunicja\\Epic Music Mix I - Game Music.mp3";
+            dzwiek_pocisku.URL = "amunicja\\strzal.mp3";
+            eksplozja.URL = "amunicja\\efekt dzwiekowy.mp3";
+
+
+            rand = new Random();
+
+            muzykaWtle.settings.setMode("loop", true);
+            muzykaWtle.settings.volume=7;
+            dzwiek_pocisku.settings.volume = 1;
+            eksplozja.settings.volume = 4;
+
+            muzykaWtle.controls.play();
+
+
+            Gwiazdy = new PictureBox[10];
+
+            for (int i = 0; i < Gwiazdy.Length; i++)
+            {
+                Gwiazdy[i] = new PictureBox();  
+                Gwiazdy[i].BorderStyle = BorderStyle.None;
+                Gwiazdy[i].Location = new Point(rand.Next(-15, 580), rand.Next(-10, 500));
+                if (i % 2 == 0)
+                {
+                    Gwiazdy[i].Size = new Size(3, 3);
+                    Gwiazdy[i].BackColor = Color.GhostWhite;
+                }
+                else if (i % 3 == 0)
+                {
+                    Gwiazdy[i].Size = new Size(2, 2);
+                    Gwiazdy[i].BackColor = Color.Yellow;
+                }
+                else
+                {
+                    Gwiazdy[i].Size = new Size(4, 4);
+                    Gwiazdy[i].BackColor = Color.DarkGray;
+                }
+                this.Controls.Add(Gwiazdy[i]);
+
+
+
+                Image obraz_przeciwnika1 = Image.FromFile("przeciwnicy\\boss 1.png");
+                Image obraz_przeciwnika2 = Image.FromFile("przeciwnicy\\boss 2.png");
+                Image obraz_przeciwnika3 = Image.FromFile("przeciwnicy\\boss 3.png");
+                Image obraz_przeciwnika4 = Image.FromFile("przeciwnicy\\boss 4.png");
+                Image obraz_przeciwnika5 = Image.FromFile("przeciwnicy\\boss 5.png");
+
+                Image obraz_pocisku = Image.FromFile("amunicja\\pocisk.png");
+
+                Przeciwnicy = new PictureBox[8];
+
+                for(int j = 0; j < Przeciwnicy.Length; j++)
+                {
+                    Przeciwnicy[j] = new PictureBox();
+                    Przeciwnicy[j].Size = new Size(45, 45);
+                    Przeciwnicy[j].SizeMode = PictureBoxSizeMode.Zoom;
+                    Przeciwnicy[j].BorderStyle = BorderStyle.None;
+                    Przeciwnicy[j].Visible = false;
+                    this.Controls.Add(Przeciwnicy[j]);
+                    Przeciwnicy[j].Location = new Point((j + 1) * 40, -5);
+                }
+
+                Przeciwnicy[0].Image = obraz_przeciwnika1;
+                Przeciwnicy[1].Image = obraz_przeciwnika3;
+                Przeciwnicy[2].Image = obraz_przeciwnika2;
+                Przeciwnicy[3].Image = obraz_przeciwnika5;
+                Przeciwnicy[4].Image = obraz_przeciwnika4; 
+                Przeciwnicy[5].Image = obraz_przeciwnika5; 
+                Przeciwnicy[6].Image = obraz_przeciwnika4; 
+                Przeciwnicy[7].Image = obraz_przeciwnika1;
+                //Przeciwnicy[8].Image = obraz_przeciwnika2; 
+                //Przeciwnicy[9].Image = obraz_przeciwnika3; 
+
+
+                Pocisk = new PictureBox[3];
+
+                for (int j = 0; j < Pocisk.Length; j++)
+                {
+                    Pocisk[j] = new PictureBox();
+                    Pocisk[j].Size = new Size(5, 5);
+                    Pocisk[j].Image = obraz_pocisku;
+                    Pocisk[j].SizeMode = PictureBoxSizeMode.Zoom;
+                    Pocisk[j].BorderStyle =BorderStyle.None;
+                    this.Controls.Add(Pocisk[j]);
+
+
+                }
+
+                Pocisk_Przeciwnika = new PictureBox[10];
+
+                for(int j = 0; j < Pocisk_Przeciwnika.Length; j++)
+                {
+                    Pocisk_Przeciwnika[j] = new PictureBox();
+                    Pocisk_Przeciwnika[j].Size = new Size(2, 20);
+                    Pocisk_Przeciwnika[j].Visible = false;
+                    Pocisk_Przeciwnika[j].BackColor = Color.Yellow;
+                    int numer_przeciwnika = rand.Next(0, 8);
+                    Pocisk_Przeciwnika[j].Location = new Point(Przeciwnicy[numer_przeciwnika].Location.X, Przeciwnicy[numer_przeciwnika].Location.Y - 5);
+                    this.Controls.Add(Pocisk_Przeciwnika[j]);
+                }
+
+                
+
+
+
+            }
+
+        }
+
+        private void Move_timer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Gwiazdy.Length / 2; i++)
+            {
+                Gwiazdy[i].Top += szybkosc_tla;
+                if (Gwiazdy[i].Top >= this.Height)
+                {
+                    Gwiazdy[i].Top = -Gwiazdy[i].Height;
+                }
+            }
+
+            for (int i = Gwiazdy.Length / 2; i < Gwiazdy.Length; i++)
+            {
+                Gwiazdy[i].Top += szybkosc_tla - 2;
+                if (Gwiazdy[i].Top >= this.Height)
+                {
+                    Gwiazdy[i].Top = -Gwiazdy[i].Height;
+                }
+            }
+        }
+
+        private void timer_prawo_Tick(object sender, EventArgs e)
+        {
+            if (pictureBoxhero_player.Left < 395)
+            {
+                pictureBoxhero_player.Left += szybkosc_gracza;
+            }
+        }
+
+        private void timer_lewo_Tick(object sender, EventArgs e)
+        {
+            if (pictureBoxhero_player.Right > 40)
+            {
+                pictureBoxhero_player.Left -= szybkosc_gracza;
+            }
+
+        }
+
+        private void timer_wGore_Tick(object sender, EventArgs e)
+        {
+            if (pictureBoxhero_player.Top > 10)
+            {
+                pictureBoxhero_player.Top -= szybkosc_gracza;
+            }
+        }
+
+        private void timer_wDol_Tick(object sender, EventArgs e)
+        {
+            if (pictureBoxhero_player.Top < 405)
+            {
+                pictureBoxhero_player.Top += szybkosc_gracza;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            timer_prawo.Stop();
+            timer_lewo.Stop();
+            timer_wDol.Stop();
+            timer_wGore.Stop();
+
+            if(e.KeyCode == Keys.Escape)
+            {
+                if (!koniec)
+                {
+                    if (pausa)
+                    {
+                        StartTimer();
+                        label.Visible = false;                     
+                        muzykaWtle.controls.play();
+                        pausa = false;
+                    }
+                    else
+                    {
+                       
+                        label.Text = "Pauza";
+                        label.BackColor = Color.Transparent;
+                        label.Visible = true;
+                        muzykaWtle.controls.stop();
+                        StopTimer();
+                        pausa = true;
+                    }
+                    
+                    
+                }
+            }
+           
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!pausa)
+            {
+                if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+                {
+                    timer_prawo.Start();
+                }
+                if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+                {
+                    timer_lewo.Start();
+                }
+                if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+                {
+                    timer_wDol.Start();
+                }
+                if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+                {
+                    timer_wGore.Start();
+                }
+            }
+            
+        }
+
+        // amunicja gracza 
+        private void timerPocisk_Tick(object sender, EventArgs e)
+        {
+            dzwiek_pocisku.controls.play();
+            for (int j = 0; j < Pocisk.Length; j++)
+            {
+                if(Pocisk[j].Top > 0)
+                {
+                    Pocisk[j].Visible = true;
+                    Pocisk[j].Top -= szybkosc_pocisku;
+
+                    kolizja();
+                }
+                else
+                {
+                    Pocisk[j].Visible = false;
+                    Pocisk[j].Location = new Point(pictureBoxhero_player.Location.X + 18, pictureBoxhero_player.Location.Y -j * 30);
+                }
+            }
+        }
+
+        private void pictureBoxhero_player_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // metoda umożliwająca ruch przediwnika 
+        private void RuchPrzeciwnika(PictureBox [] array, int speed)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Visible = true;
+                array[i].Top += speed;
+
+                if(array[i].Top > this.Height)
+                {
+                    array[i].Location = new Point((i + 1) * 40, -20);
+                }
+            }
+        }
+
+
+
+        private void timerPrzeciwnik_Tick(object sender, EventArgs e)
+        {
+            RuchPrzeciwnika(Przeciwnicy, szybkosc_przeciwnika);
+        }
+
+        // metoda kolizji, warunki zdobycia punktów oraz smierc gracza  
+        private void kolizja()
+        {
+            for (int i = 0; i < Przeciwnicy.Length; i++)
+            {
+                if (Pocisk[0].Bounds.IntersectsWith(Przeciwnicy[i].Bounds) || Pocisk[1].Bounds.IntersectsWith(Przeciwnicy[i].Bounds) || Pocisk[2].Bounds.IntersectsWith(Przeciwnicy[i].Bounds))
+                {
+                    eksplozja.controls.play();
+
+                    punkty += 1;
+                    label_punkty.Text = (punkty < 10) ? "0" + punkty.ToString() : punkty.ToString();
+
+                    if (punkty % 50 == 0)
+                    {
+                        poziom += 1;
+                        label_poziom.Text = (poziom < 10) ? "0" + poziom.ToString() : poziom.ToString();
+
+                        if(szybkosc_przeciwnika <= 20 && szybkosc_pocisk_przeciwnika <= 20 && poziom_trudnisci >= 0)
+                        {
+                            poziom_trudnisci--;
+                            szybkosc_przeciwnika++;
+                            szybkosc_pocisk_przeciwnika++;
+                        }
+                        if(poziom == 10)
+                        {
+                            KoniecGry("WYGRAŁEŚ");
+                        }
+                    }
+                    Przeciwnicy[i].Location = new Point((i + 1) * 40, -50);
+                }
+                if (pictureBoxhero_player.Bounds.IntersectsWith(Przeciwnicy[i].Bounds)) // kolizja z przeciwnikiem
+                {
+                    eksplozja.settings.volume = 30;
+                    eksplozja.controls.play();
+                    pictureBoxhero_player.Visible = false;
+                    KoniecGry("Koniec gry");
+                }
+            }
+        }
+
+        private void Kolizja_zPociskiem()  // kolizja z pociskiem - smierc gracza
+        {
+            for(int j = 0; j < Pocisk_Przeciwnika.Length; j++)
+            {
+                if (Pocisk_Przeciwnika[j].Bounds.IntersectsWith(pictureBoxhero_player.Bounds))
+                {
+                    Pocisk_Przeciwnika[j].Visible = false;
+                    eksplozja.settings.volume = 30;
+                    eksplozja.controls.play();
+                    pictureBoxhero_player.Visible = false;
+                    KoniecGry("Koniec gry");
+
+                }
+            }
+        }
+
+        // metoda ktora zakańcza gre
+        private void KoniecGry(String game_over)
+        {
+            muzykaWtle.controls.stop();
+            StopTimer();
+
+            label.Text = game_over;
+            label.Visible = true;
+            btn_powtorz.Visible = true;
+            btn_zakoncz.Visible = true;
+
+
+        }
+
+        // metoda zatrzymanie gry
+        private void StopTimer()
+        {
+            Move_timer.Stop();
+            timerPocisk.Stop();
+            timerPrzeciwnik.Stop();
+            timerPrzeciwnik_Pocisk.Stop();
+        }
+
+        // metoda rozpoczecia gry
+        private void StartTimer()
+        {
+            Move_timer.Start();
+            timerPocisk.Start();
+            timerPrzeciwnik.Start();
+            timerPrzeciwnik_Pocisk.Start();
+
+        }
+
+        private void timerPrzeciwnik_Pocisk_Tick(object sender, EventArgs e)
+        {
+            for(int j = 0; j < Pocisk_Przeciwnika.Length - poziom_trudnisci; j++)
+            {
+                if(Pocisk_Przeciwnika[j].Top < this.Height)
+                {
+                    Pocisk_Przeciwnika[j].Visible = true;
+                    Pocisk_Przeciwnika[j].Top += szybkosc_pocisk_przeciwnika;
+                    Kolizja_zPociskiem();
+                }
+                else
+                {
+                    Pocisk_Przeciwnika[j].Visible = false;
+                    int numer_przeciwnika = rand.Next(0, 8);
+                    Pocisk_Przeciwnika[j].Location = new Point(Przeciwnicy[numer_przeciwnika].Location.X + 20, Przeciwnicy[numer_przeciwnika].Location.Y + 30);
+                }
+            }
+                
+        }
+
+        // działanie przecisku-powtórz
+        private void btn_powtorz_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            InitializeComponent();
+            Form1_Load(e, e);
+        }
+
+        // działanie przecisku-zakończ działanie aplikacji 
+        private void btn_zakoncz_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
